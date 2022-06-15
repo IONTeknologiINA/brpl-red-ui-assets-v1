9ba1439fd584fbfa566065d2085a6e8733cec1f0
Annas Dan <annassdan@gmail.com>
2022-06-16 00:26:18 +0700

    add comment for build config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  // enable below if wanna use for production
-  publicPath: 'https://cdn.jsdelivr.net/',
-  assetsDir: 'gh/annassdan/red/dist/'
+  // enable below for use on production and add this to index.html <base href="https://cdn.jsdelivr.net/">
+  // publicPath: 'https://cdn.jsdelivr.net/',
+  // assetsDir: 'gh/annassdan/red-staging/dist/'
 })
